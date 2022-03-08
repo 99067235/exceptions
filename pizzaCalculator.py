cf0c93dc20315a0717bb80e52897b5d9fecc0a22
unknown <99067235@mydavinci.nl>
2022-03-08 10:58:03 +0100

    pizzacalculator afgemaakt

--- pizzaCalculator.py
+++ pizzaCalculator.py
@@ -14,25 +14,39 @@ print("""Prijzen in euro's
 small = 3.99
 medium = 5
 large = 6""")
+
+def kassabon():
+    my_formatter = "{0:.2f}"
+    formatted_total = my_formatter.format(aantalSmall * small + aantalMedium * medium + aantalLarge * large)
+    print("--------------------------------------")
+    print("| Uw bestelling:")
+    print(f"| {aantalSmall} small: ","€", formatted_prijssmall)
+    print(f"| {aantalMedium} medium: €", formatted_prijsmedium)
+    print(f"| {aantalLarge} large: ","€",formatted_prijslarge)
+    print("|")
+    print("| Totaalprijs = €", formatted_total)
+    print("--------------------------------------")
 def pizza():
+    my_formatter = "{0:.2f}"
+    global aantalSmall, aantalMedium, aantalLarge, formatted_prijslarge, formatted_prijsmedium, formatted_prijssmall
     try:
         aantalSmall = float(input("Hoeveel small pizza's wil je? "))
-        print("Dit kost", aantalSmall * small)
+        prijssmall = aantalSmall * small
+        formatted_prijssmall = my_formatter.format(prijssmall)
+        print("Dit kost", formatted_prijssmall)
 
         aantalMedium = float(input("Hoeveel medium pizza's wil je? "))
-        print("Dit kost", aantalMedium * medium)
+        prijsmedium = aantalMedium * medium
+        formatted_prijsmedium = my_formatter.format(prijsmedium)
+        print("Dit kost", formatted_prijsmedium)
 
         aantalLarge = float(input("Hoeveel large pizza's wil je? "))
-        print("Dit kost", aantalLarge * large)
+        prijslarge = aantalLarge * large
+        formatted_prijslarge = my_formatter.format(prijslarge)
+        print("Dit kost", formatted_prijslarge)
 
-        print("--------------------------------------")
-        print("| Uw bestelling:")
-        print(f"| {aantalMedium} medium: €", aantalMedium * medium)
-        print(f"| {aantalSmall} small: ","€", aantalSmall * small)
-        print(f"| {aantalLarge} large: ","€", aantalLarge * large)
-        print("|")
-        print("| Totaalprijs = €", aantalSmall * small + aantalMedium * medium + aantalLarge * large)
-        print("--------------------------------------")
+        kassabon()
+
         
     except ValueError:
         print("Vul a.u.b. een getal in.")
